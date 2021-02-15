bc7cc2d457ea03b3a78d6ba306502df55220ba0d
cirocki <marcincirocki89@gmail.com>
2021-02-15 22:23:55 +0100

    Add example page - template

--- src/pages/example.js
+++ src/pages/example.js
@@ -6,19 +6,18 @@ import Layout from "../components/layout/Layout"
 const StyledMainWrapper = styled.main`
   padding-top: 80px;
 `
-
 const StyledHeader = styled.h2`
   font-family: ${props => props.theme.fonts.secondary};
   color: ${props => props.theme.colors.accent};
   font-size: 3rem;
 `
 
-export default function Home() {
+export default function About() {
   return (
     <Layout>
       <StyledMainWrapper>
-        <SEO title="Home" description="My custom starter in Gatsby.js" />
-        <StyledHeader>My Gatsby Starter Index</StyledHeader>
+        <SEO title="Page Title" description="Page description." />
+        <StyledHeader>Page Header</StyledHeader>
       </StyledMainWrapper>
     </Layout>
   )
